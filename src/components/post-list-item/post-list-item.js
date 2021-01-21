9dd4ee030292c69da687bf3ddc2f7bab4027d927
noiseLim <noiselim@gmail.com>
2021-01-21 16:44:11 +0300

    refactoring attempt

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,46 +1,43 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import './post-list-item.css';
 
-export default class PostListItem extends Component {
+function PostListItem({onToggleLiked, label, onToggleImportant, onDelete, important, like}) {
 
-    render() {
-        const {label, onDelete, onToggleImportant, onToggleLiked, important, like} = this.props;
-        // const {important, like} = this.state;
-        let classNames = "app-list-item d-flex justify-content-between";
+    let classNames = "app-list-item d-flex justify-content-between";
 
-        if (important) {
-            classNames += " important";
-        }
-
-        if (like) {
-            classNames += " like";
-        }
-
-
-        return (
-            <div className={classNames}>
-                <span
-                className="app-list-item-label"
-                onClick={onToggleLiked}>
-                    {label}
-                </span>
-                <div className="d-flex justify-content-center align-items-center">
-                    <button
-                        type="button"
-                        className="btn-star btn-sm"
-                        onClick={onToggleImportant}>
-                            <i className="fa fa-star"></i>
-                    </button>
-                    <button
-                        type="button"
-                        className="btn-trash btn-sm"
-                        onClick={onDelete}>
-                            <i className="fa fa-trash-o"></i>
-                    </button>
-                    <i className="fa fa-heart"></i>
-                </div>
-            </div>
-        )
+    if (important) {
+        classNames += " important";
     }
+
+    if (like) {
+        classNames += " like";
+    }
+
+    return (
+        <div className={classNames}>
+            <span
+            className="app-list-item-label"
+            onClick={onToggleLiked}>
+                {label}
+            </span>
+            <div className="d-flex justify-content-center align-items-center">
+                <button
+                    type="button"
+                    className="btn-star btn-sm"
+                    onClick={onToggleImportant}>
+                        <i className="fa fa-star"></i>
+                </button>
+                <button
+                    type="button"
+                    className="btn-trash btn-sm"
+                    onClick={onDelete}>
+                        <i className="fa fa-trash-o"></i>
+                </button>
+                <i className="fa fa-heart"></i>
+            </div>
+        </div>
+    )
 }
+
+export default PostListItem;
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,36 +1,3 @@
-// import React, {useState, useEffect} from 'react';
-
-// import './search-panel.css';
-
-// function SearchPanel({onUpdateSearch}) {
-
-//     const [term, updTerm] = useState('');
-
-//     useEffect(() => {
-//         onUpdateSearcher();
-//     }, [])
-//     console.log(term);
-
-//     const onUpdateSearcher = (e) => {
-//         // const getTerm = e.target.value;
-//         // updTerm(getTerm);
-//         onUpdateSearch(term);
-//     }
-
-//     return (
-//         <input
-//             className="form-control search-input"
-//             type="text"
-//             placeholder="Поиск по записям"
-//             // onChange={onUpdateSearcher}
-//             onChange={e => updTerm(e.target.value)}
-//         />
-//     )
-// }
-
-// export default SearchPanel;
-
-
 import React, { Component } from 'react';
 
 import './search-panel.css';
